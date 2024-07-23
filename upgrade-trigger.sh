91172a9e515d742c5af734dc7c62ee608466b4bf
pulsar <153952590+pulsar-node@users.noreply.github.com>
2024-07-23 04:07:28 +0200

    Update upgrade-trigger.sh

--- upgrade-trigger.sh
+++ upgrade-trigger.sh
@@ -1,1 +1,1 @@
-screen -S home -d -m bash -c "bash <(curl -s https://raw.githubusercontent.com/pulsar-node/utils/main/blockheight_upgrade.sh) -n oraichain -i Oraichain -t 21627705 -v v0.42.1 -b oraid"
+screen -S home -d -m bash -c "bash <(curl -s https://raw.githubusercontent.com/pulsar-node/utils/main/blockheight_upgrade.sh) -n oraichain -i Oraichain -t 28107890 -v v0.42.1 -b oraid"
