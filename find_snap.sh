c40f97c49d884410e4a9b148401e11adc66472c9
pulsar <153952590+pulsar-node@users.noreply.github.com>
2024-08-07 07:50:20 +0200

    Update find_snap.sh

--- find_snap.sh
+++ find_snap.sh
@@ -2,8 +2,8 @@
 
 # List of URLs to check
 urls=(
-    "https://files.andromedanode.co/$PROJECT/snap_$PROJECT.tar.lz4"
     "https://snapshots.polkachu.com/snapshots/orai/orai_29408148.tar.lz4"
+    "https://files.andromedanode.co/$PROJECT/snap_$PROJECT.tar.lz4"
     "https://snapshots-$NETWORK.nodejumper.io/$PROJECT-$NETWORK/$PROJECT-$PROJECT_latest.tar.lz4"
     "https://snap.nodex.one/$PROJECT-$NETWORK/$PROJECT-latest.tar.lz4"
     "https://snapshots.kjnodes.com/$PROJECT-$NETWORK/snapshot_latest.tar.lz4"
