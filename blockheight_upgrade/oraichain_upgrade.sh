616f340c172f94ed0cd4f2d960c3f1c1fbc92c1c
pulsar <153952590+pulsar-node@users.noreply.github.com>
2024-08-10 15:40:43 +0200

    Update oraichain_upgrade.sh

--- blockheight_upgrade/oraichain_upgrade.sh
+++ blockheight_upgrade/oraichain_upgrade.sh
@@ -2,13 +2,13 @@
 
 sudo systemctl stop oraid
 
+VERSION=$1
 # Download new binary
 cd $HOME 
 rm -rf orai 
 git clone https://github.com/oraichain/orai 
 cd orai/orai 
-git checkout v0.42.2
-make install
-
+git checkout $VERSION
+# make install
+echo $VERSION
 sudo systemctl start oraid
-
