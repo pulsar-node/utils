ce00b2a2af1bf1c60ae48fc61f96cb976a635be7
pulsar <153952590+pulsar-node@users.noreply.github.com>
2024-08-14 16:03:30 +0200

    Update find_snap.sh

--- find_snap.sh
+++ find_snap.sh
@@ -9,6 +9,7 @@ urls=(
     "https://chains.oldcat.io/mainnets/orai/orai_29985734.tar.lz4"
     "https://snapshots.nysa.network/Oraichain/Oraichain_29940329.tar.lz4"
     "https://files.andromedanode.co/$PROJECT/snap_$PROJECT.tar.lz4"
+    "https://files.node4all.nl/$PROJECT/snap_$PROJECT.tar.lz4"
     # Add more URLs here if needed
 )
 
