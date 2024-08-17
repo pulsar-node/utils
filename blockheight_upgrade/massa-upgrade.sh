0f8d45fdd12ea9b03b538a49bf02ff329c77e85d
pulsar <153952590+pulsar-node@users.noreply.github.com>
2024-08-17 13:18:26 +0200

    Update massa-upgrade.sh

--- blockheight_upgrade/massa-upgrade.sh
+++ blockheight_upgrade/massa-upgrade.sh
@@ -1,1 +1,11 @@
 
+# update and init app
+export VERSION=2.4
+URL="https://github.com/massalabs/massa/releases/download/MAIN.${VERSION}/massa_MAIN.${VERSION}_release_linux.tar.gz"
+wget "${URL}" -O $HOME/massa_MAIN.${VERSION}_release_linux.tar.gz
+tar -xvf $HOME/massa_MAIN.${VERSION}_release_linux.tar.gz -C $HOME/
+chmod +x $HOME/massa/massa-node/massa-node $HOME/massa/massa-client/massa-client 
+sudo systemctl restart massad && sudo journalctl -u massad -f -o cat
+
+# start client
+cd ~/massa/massa-client && ./massa-client -p "${PASSWORD}"
