beaf8e2b2769bd110207a12fa00e93e5f50ae5c8
pulsar <153952590+pulsar-node@users.noreply.github.com>
2024-08-17 16:34:22 +0200

    Update snap-finder.sh

--- snap-finder.sh
+++ snap-finder.sh
@@ -6,9 +6,9 @@
 # List of URLs to check 
 urls=(
     "https://snapshots.polkachu.com/snapshots/orai/orai_30369412.tar.lz4"
+    "https://snapshots.nysa.network/Oraichain/Oraichain_29940329.tar.lz4"
     "https://files.andromedanode.co/$PROJECT/snap_$PROJECT.tar.lz4"
     "https://chains.oldcat.io/mainnets/orai/orai_29985734.tar.lz4"
-    "https://snapshots.nysa.network/Oraichain/Oraichain_29940329.tar.lz4"
     "https://files.node4all.nl/$PROJECT/snap_$PROJECT.tar.lz4"
     # Add more URLs here if needed
 )
