8da0067c26411f0214ff7843a5665fe392cf5aa7
pulsar <153952590+pulsar-node@users.noreply.github.com>
2024-09-30 13:14:37 +0200

    Update blockheight_upgrade.sh

--- blockheight_upgrade.sh
+++ blockheight_upgrade.sh
@@ -33,7 +33,7 @@ while sleep 5; do
     break
   else
     rest=$(expr $TARGET_BLOCK - $height)
-    h=$(($rest * 0.80 / 3600))
+    h=$(($rest * 80 / 100 / 3600))
     m=$(($rest % 3600 / 60))
     s=$(($rest % 3600 % 60))
     printf "Current block height: %s - %s %02d:%02d:%02d   \r" $(printYellow ${height}) "$rest" "$h" "$m" "$s"
