ae7669e44f161e9da92ad1455e1374d8ae4b6788
pulsar <153952590+pulsar-node@users.noreply.github.com>
2024-10-18 10:37:11 +0200

    Update go_installer.sh

--- go_installer.sh
+++ go_installer.sh
@@ -29,7 +29,7 @@ rm "${VERSION}.${OS}-${ARCH}.tar.gz"
 
 # Update PATH and GOPATH
 if ! grep -q "/usr/local/go/bin" "$HOME/.bash_profile"; then
-  echo "export PATH=\$PATH:/usr/local/go/bin" >> "$HOME/.bash_profile"
+  echo "export PATH=\$PATH:/usr/local/go/bin:~/go/bin" >> "$HOME/.bash_profile"
 fi
 if ! grep -q "GOPATH=\$HOME/go" "$HOME/.bash_profile"; then
   echo "export GOPATH=\$HOME/go" >> "$HOME/.bash_profile"
@@ -37,9 +37,6 @@ fi
 
 # Ensure .bash_profile exists
 [ ! -f ~/.bash_profile ] && touch ~/.bash_profile
-
-# Add Go binary directories to PATH and create ~/go/bin directory
-echo "export PATH=\$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
 [ ! -d ~/go/bin ] && mkdir -p ~/go/bin
 
 # Source .bash_profile to apply changes
