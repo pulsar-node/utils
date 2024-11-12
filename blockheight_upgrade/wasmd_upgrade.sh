e024a808c14bef8aaa5760e53ea4cbe7debe22fb
pulsar <153952590+pulsar-node@users.noreply.github.com>
2024-11-12 14:46:01 +0100

    Update wasmd_upgrade.sh

--- blockheight_upgrade/wasmd_upgrade.sh
+++ blockheight_upgrade/wasmd_upgrade.sh
@@ -4,7 +4,7 @@ sudo systemctl stop oraid
 cd $HOME 
 git clone https://github.com/oraichain/wasmd
 cd wasmd
-git checkout v0.50.0
+git checkout $1  #v0.xx.x
 # NOTE: you may need to upgrade your Golang version to 1.22.6+ with GOTOOLCHAIN to build the binary
 make build
 oraid version
