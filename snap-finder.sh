9282eb0b35ddbf817fa231f2fd26c02f67d84395
pulsar <153952590+pulsar-node@users.noreply.github.com>
2024-11-14 09:58:16 +0100

    Update snap-finder.sh

--- snap-finder.sh
+++ snap-finder.sh
@@ -4,27 +4,28 @@
 # curl -o - -L https://ss.pryzm.nodestake.org/${SNAP_NAME}  | lz4 -c -d - | tar -x -C $HOME/.pryzm
 
 # List of URLs to check 
-urls=`curl -s https://snapshot.owallet.io/files.json | jq -r '.files | max_by(.mtime) | .filename' | awk '{print "https://snapshot.owallet.io/orai/" $0}'`
-urls=+(
-    "https://snapshots.polkachu.com/snapshots/orai/orai_30369412.tar.lz4"
-    "https://chains.oldcat.io/mainnets/orai/orai_29985734.tar.lz4"
+owallet=$(curl -s https://snapshot.owallet.io/files.json | jq -r '.files | max_by(.mtime) | .filename' | awk '{print "https://snapshot.owallet.io/orai/" $0}')
+polkachu=$(curl -s https://www.polkachu.com/tendermint_snapshots/orai | grep -oE 'https://[^"]+orai_[0-9]+\.tar\.lz4' | sort | uniq | head -n 1)
+
+urls=(
+    "$owallet"
+    "$polkachu"
     "https://files.andromedanode.co/$PROJECT/snap_$PROJECT.tar.lz4"    
-    "https://files.node4all.nl/$PROJECT/snap_$PROJECT.tar.lz4"
-    # Add more URLs here if needed
-)
+    )
 
 # Iterate over each URL
 for url in "${urls[@]}"; do
     if curl -s --head "$url" | head -n 1 | grep "200" > /dev/null; then
         # If the URL returns status 200, download and extract the archive
-        # if curl "$url" | lz4 -dc - | tar -xf - -C "$DIRECTORY"; then
+        if curl "$url" | lz4 -dc - | tar -xf - -C "$DIRECTORY"; then
             echo "Download and extraction successful from: $url"
-        #     # Exit loop since download was successful
-        #     break
-        # else
-        #     echo "Failed to extract archive from: $url"
-        # fi
+            # Exit loop since download was successful
+            break
+        else
+            echo "Failed to extract archive from: $url"
+        fi
     else
         echo "URL $url is not available."
     fi
 done
+
