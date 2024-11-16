f909bcfdcda3611fc9a5388044f9aaecd3d942fc
pulsar <153952590+pulsar-node@users.noreply.github.com>
2024-11-16 13:53:04 +0100

    Update find-snap.sh

--- find-snap.sh
+++ find-snap.sh
@@ -9,7 +9,7 @@ polkachu=$(curl -s https://www.polkachu.com/tendermint_snapshots/orai | grep -oE
 
 urls=(
     "$owallet"
-    "https://files.andromedanode.co/$PROJECT/snap_$PROJECT.tar.lz4"
+    "https://files.andromedanode.co/oraichain/snap_oraichain.tar.lz4"
     "$polkachu"
     )
 
