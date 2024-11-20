3c5e8599af00ec8881a14f720a57a56352073ea3
pulsar <153952590+pulsar-node@users.noreply.github.com>
2024-11-20 09:34:16 +0100

    Update oraid_upgrade.sh

--- blockheight_upgrade/oraid_upgrade.sh
+++ blockheight_upgrade/oraid_upgrade.sh
@@ -9,7 +9,6 @@ git clone https://github.com/oraichain/orai
 cd orai
 git checkout $1   # version v0.xx.x
 cd orai
-# NOTE: you may need to upgrade your Golang version to 1.22.6+ with GOTOOLCHAIN to build the binary
 make install
 oraid version
 sudo systemctl start oraid
