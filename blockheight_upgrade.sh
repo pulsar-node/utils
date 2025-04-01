07d09bf9d149013d79fa628c174460d2654d0e42
pulsar <153952590+pulsar-node@users.noreply.github.com>
2025-04-01 07:56:33 +0200

    Update blockheight_upgrade.sh

--- blockheight_upgrade.sh
+++ blockheight_upgrade.sh
@@ -41,7 +41,7 @@ while sleep 5; do
   else
     rest=$(expr $TARGET_BLOCK - $height)
     #ms=$(($rest * 70 / 100))
-    ms=$(($rest * 0.70))
+    ms=$(($rest * 0.7))
     h=$(($ms / 3600))
     m=$(($ms % 3600 / 60))
     s=$(($ms % 3600 % 60))
