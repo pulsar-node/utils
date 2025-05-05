6eba2d8799b2a0d58d61c4711d0e2c2910b76317
pulsar <153952590+pulsar-node@users.noreply.github.com>
2025-05-05 23:06:56 +0200

    Update oraid_upgrade.sh

--- blockheight_upgrade/oraid_upgrade.sh
+++ blockheight_upgrade/oraid_upgrade.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 sudo systemctl stop oraid
-
 cd $HOME/wasmd
 git pull
-git checkout $1   # version v0.xx.x
+git checkout $1   # version v0.50.xx
 make build
 oraid version --long | grep -e commit -e version
-
 sudo systemctl restart oraid
