bf976196c2f86c3e7ea72a23a9db25cd5f858195
pulsar <153952590+pulsar-node@users.noreply.github.com>
2025-05-05 12:10:47 +0200

    Update find-snap.sh

--- find-snap.sh
+++ find-snap.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# SNAP_NAME=$(curl -s https://ss.pryzm.nodestake.org/ | egrep -o ">20.*\.tar.lz4" | tr -d ">")
-# curl -o - -L https://ss.pryzm.nodestake.org/${SNAP_NAME}  | lz4 -c -d - | tar -x -C $HOME/.pryzm
-
 # List of URLs to check 
 owallet=$(curl -s https://snapshot.owallet.io/files.json | jq -r '.files | max_by(.mtime) | .filename' | awk '{print "https://snapshot.owallet.io/orai/" $0}')
 polkachu=$(curl -s https://www.polkachu.com/tendermint_snapshots/orai | grep -oE 'https://[^"]+orai_[0-9]+\.tar\.lz4' | sort | uniq | head -n 1)
