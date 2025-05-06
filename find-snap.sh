f6196515262566e3f7a38e5efceabf2b33dd7a4b
pulsar <153952590+pulsar-node@users.noreply.github.com>
2025-05-06 12:39:57 +0200

    Update find-snap.sh

--- find-snap.sh
+++ find-snap.sh
@@ -2,17 +2,14 @@
 
 # List of URLs to check 
 owallet=$(curl -s https://snapshot.owallet.io/files.json | jq -r '.files | max_by(.mtime) | .filename' | awk '{print "https://snapshot.owallet.io/orai/" $0}')
-polkachu=$(curl -s https://www.polkachu.com/tendermint_snapshots/orai | grep -oE 'https://[^"]+orai_[0-9]+\.tar\.lz4' | sort | uniq | head -n 1)
 
 # curl -s https://snapshot.orai.io
 # oraichain="https://orai.s3.us-east-2.amazonaws.com/hourly_snapshots/oraichain_46073178.tar.lz4"
 
 urls=(
     "$owallet"
+    "https://snap.blockval.io/oraichain/oraichain_latest.tar.lz4"
     "https://snapshot.pfc.zone/files/oraichain/oraichain.latest.tar.lz4"
-    "http://snapshots.autostake.com/lyIs25DaSWMSm8evWKHGQrb/Oraichain/latest.tar.lz4"
-    "https://snap.blockval.io/oraichain/oraichain_latest.tar.lz4"    
-    "$polkachu"    
 )
 
 # Iterate over each URL
