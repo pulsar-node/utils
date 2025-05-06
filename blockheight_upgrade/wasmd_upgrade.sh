8d6c100e8ba2ab2c6f591533a28cf30588a26d60
pulsar <153952590+pulsar-node@users.noreply.github.com>
2025-05-06 12:24:09 +0200

    Update wasmd_upgrade.sh

--- blockheight_upgrade/wasmd_upgrade.sh
+++ blockheight_upgrade/wasmd_upgrade.sh
@@ -2,10 +2,11 @@
 
 sudo systemctl stop oraid
 cd $HOME
+cp $HOME/go/bin/oraid $HOME/go/bin/oraid.bak
 rm -rf wasmd 
 git clone https://github.com/oraichain/wasmd
 cd wasmd
-git checkout $1  #v0.50.x
+git checkout $1  #v0.50.xx
 make build
 oraid version
 sudo systemctl restart oraid
