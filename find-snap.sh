fc7ef08d76ea377e3837a18bc3649aff70780a0b
pulsar <153952590+pulsar-node@users.noreply.github.com>
2026-08-15 18:30:01 +0200

    Update find-snap.sh

--- find-snap.sh
+++ find-snap.sh
@@ -4,6 +4,7 @@
 # oraichain="https://orai.s3.us-east-2.amazonaws.com/hourly_snapshots/oraichain_46073178.tar.lz4"
 OWallet=$(curl -s https://snapshot.owallet.io/files.json | jq -r '.files | max_by(.mtime) | .filename' | awk '{print "https://snapshot.owallet.io/orai/" $0}')
 ORAICHAIN="https://orai.s3.us-east-2.amazonaws.com/$(curl -fsSL https://snapshot.orai.io/snapshot.json | jq -r '.[0].Key')"
+echo "$ORAICHAIN"
 
 urls=(
     "$ORAICHAIN"
